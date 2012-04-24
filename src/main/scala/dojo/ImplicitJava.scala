4ccfdd7a923026b34e53558c8d5c7229ad6adda0
Peter Crotty <peter@crotty.co.uk>
2012-04-24 14:18:16 +0100

    My solutions for tests without making Extended User have a constructor which takes in a User.

--- src/main/scala/dojo/ImplicitJava.scala
+++ src/main/scala/dojo/ImplicitJava.scala
@@ -4,6 +4,14 @@ import com.google.common.base.Predicate
 
 object ImplicitJava {
 
-  def funcToPred(function: (User) => Boolean) : Predicate[User] = null
+  implicit def funcToPred(function: (User) => Boolean) : Predicate[User] = {
+    new Predicate[User] {
+      def apply(p1: User): Boolean = function(p1)
+    }
+  }
+  
+  implicit def userToExtendedUser(user:User) : ExtendedUser = {
+    new ExtendedUser(user.getId, user.getName, user.isMale, user.getAge)
+  }
 
 }
--- src/main/scala/dojo/UserLookup.scala
+++ src/main/scala/dojo/UserLookup.scala
@@ -1,6 +1,8 @@
 package dojo
 
 import java.util.{ArrayList, List}
+import ImplicitJava._
+import scala.collection.JavaConversions._
 
 class UserLookup(dataSource :DataSource) extends JUserLookup {
 
@@ -9,15 +11,29 @@ class UserLookup(dataSource :DataSource) extends JUserLookup {
      can you do it in the Scala way by passing in a function?
      Is there a way to imlicit[ly] convert a function to a Predicate?
    */
-  def olderThan(age :Int): List[User] = new ArrayList[User]()
+  def olderThan(age :Int): List[User] = {
+    dataSource
+      .findUsers((user:User) => (user.getAge > age))
+  }
 
   /*
       Are there standard JavaConversions to make it easier to work with Java collections?
    */
-  def namesYoungerThan(age: Int):List[String] = new ArrayList[String]()
+  def namesYoungerThan(age: Int):List[String] = {   null
+    dataSource
+      .findUsers((user:User) => (user.getAge < age))
+      .map(item => item.getName)
+  }
 
-  def allFemale(): List[String] = new ArrayList[String]()
+  def allFemale(): List[String] = {
+    dataSource
+      .findUsers((user:User) => (!user.isMale))
+      .map(item => item.getName)
+  }
 
-  def allEligible() = new ArrayList[User]()
+  def allEligible() = {
+    dataSource
+      .findUsers((user:User) => user.isEligible)
+  }
 
 }
