0fd4558c9a0c446dfb17967c7dd23598866e5561
Cédric CORBIERE <corbic@gmail.com>
2012-05-09 00:40:57 +0200

    First test is okay

--- src/main/scala/dojo/ImplicitJava.scala
+++ src/main/scala/dojo/ImplicitJava.scala
@@ -4,6 +4,8 @@ import com.google.common.base.Predicate
 
 object ImplicitJava {
 
-  def funcToPred(function: (User) => Boolean) : Predicate[User] = null
+  def funcToPred(function: (User) => Boolean) : Predicate[User] = new Predicate[User]{
+    def apply(input:User) = function(input)
+  }
 
 }
